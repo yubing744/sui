855f4a26af5708a4159fe1583606dfdd2d3e8242
Andrew Burnie <apburnie@hotmail.co.uk>
2022-03-03 16:21:42 +0000

    change search bar placeholder text

--- explorer/client/src/components/search/Search.tsx
+++ explorer/client/src/components/search/Search.tsx
@@ -45,7 +45,7 @@ function Search() {
             <input
                 className={styles.searchtext}
                 id="search"
-                placeholder="Search transactions by ID"
+                placeholder="Search transactions, objects or addresses by ID"
                 value={input}
                 onChange={handleTextChange}
                 type="text"
