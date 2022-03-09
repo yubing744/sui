022fd6bb9660c4d0262f4b94420b0626164d7726
Andrew Burnie <apburnie@hotmail.co.uk>
2022-03-09 18:08:43 +0000

    initial API set up

--- explorer/client/src/utils/utility_functions.tsx
+++ explorer/client/src/utils/utility_functions.tsx
@@ -1,11 +1,17 @@
 import mockTransactionData from './mock_data.json';
 
-const findDataFromID = (targetID: string | undefined, state: object | unknown) => 
-        state || mockTransactionData.data.find(({ id }) => id === targetID);
+// API info
+// const BACKEND_URL = "http://3.229.149.13:5000";
+// const ADDRESS_URL = `${BACKEND_URL}/objects?address=`;
+// const OBJECT_URL = `${BACKEND_URL}/object_info?objectId=`;
+
+const findDataFromID = (targetID: string | undefined, state: any) =>
+    state?.category !== undefined
+        ? state
+        : mockTransactionData.data.find(({ id }) => id === targetID);
 
 const navigateWithUnknown = (input: string, navigate: Function) => {
-    const data = mockTransactionData.data.find(({ id }) => id === input);
-
+    const data = findDataFromID(input, false);
     if (data === undefined || !('category' in data)) {
         navigate(`../missing/${input}`);
     } else if (data.category === 'transaction') {
