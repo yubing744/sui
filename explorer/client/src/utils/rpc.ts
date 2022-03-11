a26c21d15560a19b338871a3aaee3c98de93ebf7
Stella Cannefax <stella@mystenlabs.com>
2022-03-11 12:30:57 -0800

    add more RPC json types

--- explorer/client/src/utils/rpc.ts
+++ explorer/client/src/utils/rpc.ts
@@ -1,11 +1,8 @@
-import { Url } from "url"
-import { ExplorerObjData } from "../pages/object-result/ObjectResult";
-
 
 class SuiRpcClient {
-
     public host: string;
 
+    // TODO - stronger type for host
     public constructor(host: string) {
         this.host = host;
     }
@@ -21,12 +18,18 @@ class SuiRpcClient {
             case 200: {
                 return response.body?.getReader().read();
             }
+            default:
+                console.warn(response);
         }
     }
 }
 
+
 export type BoolString = "true" | "false";
 export type OwnerField = { ObjectOwner: number[] }
+export type JsonBytes = { bytes: number[] }
+export type JsonHexBytes = { bytes: string }
+export type AnyVec = { vec: any[] }
 
 export interface ObjectInfoResponse<T> {
     owner: string;
@@ -43,23 +46,48 @@ export interface Data<T> {
     tx_digest: number[];
 }
 
-/*
-export interface Monster {
+
+// demo-specific types below here
+export type CosmeticOption = AnyVec | RawCosmetic;
+
+export interface RawCosmetic {
+    cosmetic_type: number;
+    id: string;
+}
+
+export interface RawMonster {
     affection_level: number;
-    applied_monster_cosmetic_0: AppliedMonsterCosmetic0;
-    applied_monster_cosmetic_1: AppliedMonsterCosmetic1;
+    applied_monster_cosmetic_0: CosmeticOption;
+    applied_monster_cosmetic_1: CosmeticOption;
     breed: number;
     buddy_level: number;
     hunger_level: number;
-    id: Id;
+    id: string;
     monster_affinity: number;
-    monster_description: MonsterDescription;
+    monster_description: JsonBytes;
     monster_img_index: number;
     monster_level: number;
-    monster_name: MonsterName;
+    monster_name: JsonBytes;
     monster_xp: number;
 }
-*/
+
+export interface RawPetMonsters {
+    child_id: JsonHexBytes;
+    parent_id: JsonHexBytes;
+}
+
+export interface RawFarm {
+    applied_farm_cosmetic_0: CosmeticOption;
+    applied_farm_cosmetic_1: CosmeticOption;
+    current_xp: number;
+    farm_img_index: number;
+    farm_name: JsonBytes;
+    id: string;
+    level: number;
+    occupied_monster_slots: number;
+    pet_monsters: RawPetMonsters;
+    total_monster_slots: number;
+}
 
 
 export { SuiRpcClient }
