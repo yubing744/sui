72edcc2e9633189a4f0337ce0eda3ce7735d218e
Stella Cannefax <stella@mystenlabs.com>
2022-03-16 20:23:23 -0700

    change navigateWithUnknown to use real data

--- explorer/client/src/utils/utility_functions.tsx
+++ explorer/client/src/utils/utility_functions.tsx
@@ -1,5 +1,5 @@
 import mockTransactionData from './mock_data.json';
-import { SuiRpcClient } from './rpc';
+import { DefaultRpcClient as rpc } from './rpc';
 
 // API info
 // const BACKEND_URL = "http://3.229.149.13:5000";
@@ -12,7 +12,21 @@ const findDataFromID = (targetID: string | undefined, state: any) =>
         : mockTransactionData.data.find(({ id }) => id === targetID);
 
 
-const navigateWithUnknown = (input: string, navigate: Function) => {
+const navigateWithUnknown = async (input: string, navigate: Function) => {
+    // feels crude to just search each category for an ID, but works for now
+    console.log('searching for ' + input);
+
+    rpc.getAddressObjects(input).then(data => {
+        console.log('address found:', data);
+        if (data.objects.length > 0)
+            navigate(`../addresses/${input}`, { state: data });
+    });
+    rpc.getObjectInfo(input).then(data => {
+        console.log('object found:', data);
+        navigate(`../objects/${input}`, { state: data });
+    });
+
+    /*
     const data = findDataFromID(input, false);
     if (data === undefined || !('category' in data)) {
         navigate(`../missing/${input}`);
@@ -25,6 +39,7 @@ const navigateWithUnknown = (input: string, navigate: Function) => {
     } else {
         navigate(`../missing/${input}`);
     }
+    */
 };
 
 const logResult = function logResult<T>(task: () => Promise<T>) {
