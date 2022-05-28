25645a4ffd7de77d32f8ef5990b8507993b00fb4
Stella Cannefax <stella@mystenlabs.com>
2022-05-28 03:08:06 -0500

    DisplayBox changes for image mod work

--- explorer/client/src/components/displaybox/DisplayBox.tsx
+++ explorer/client/src/components/displaybox/DisplayBox.tsx
@@ -1,15 +1,21 @@
 // Copyright (c) 2022, Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect, useContext } from 'react';
 
+import { NetworkContext } from '../../context';
+import { ImageModClient } from '../../utils/imageModeratorClient';
 import { processDisplayValue } from '../../utils/stringUtils';
 
 import styles from './DisplayBox.module.css';
 
-function DisplayBox({ display }: { display: string | { bytes: number[] } }) {
+function DisplayBox({ display }: { display: string }) {
     const [hasDisplayLoaded, setHasDisplayLoaded] = useState(false);
     const [hasFailedToLoad, setHasFailedToLoad] = useState(false);
+
+    const [hasImgBeenChecked, setHasImgBeenChecked] = useState(false);
+    const [imgAllowState, setImgAllowState] = useState(false);
+    const [network] = useContext(NetworkContext);
 
     const imageStyle = hasDisplayLoaded ? {} : { display: 'none' };
     const handleImageLoad = useCallback(
@@ -19,7 +25,31 @@ function DisplayBox({ display }: { display: string | { bytes: number[] } }) {
 
     useEffect(() => {
         setHasFailedToLoad(false);
-    }, [display]);
+        setHasImgBeenChecked(false);
+        setImgAllowState(false);
+        console.log(
+            `start DisplayBox image content check, network: ${network}`
+        );
+
+        new ImageModClient(network)
+            .checkImage(processDisplayValue(display))
+            .then(({ ok }) => {
+                //setHasImgBeenChecked(true);
+                setImgAllowState(ok);
+                console.log(
+                    `image allow check for ${display} returned:  ${ok}`
+                );
+            })
+            .catch((error) => {
+                console.error('image content check threw', error);
+                //setHasImgBeenChecked(true);
+                // default to allow, so a broken img check service doesn't break NFT display
+                setImgAllowState(true);
+            })
+            .finally(() => {
+                setHasImgBeenChecked(true);
+            });
+    }, [display, network]);
 
     const handleImageFail = useCallback(
         (error) => {
@@ -30,13 +60,34 @@ function DisplayBox({ display }: { display: string | { bytes: number[] } }) {
         [setHasFailedToLoad]
     );
 
+    console.log(
+        `img allow state: ${imgAllowState}`,
+        `checked state: ${imgAllowState}, has display loaded: ${hasDisplayLoaded}`
+    );
+
+    // if we've loaded the display image but the check hasn't returned, display a blurry version
+    const shouldBlur = hasDisplayLoaded && !hasImgBeenChecked && !imgAllowState;
+    if (shouldBlur)
+        console.log(
+            'SHOULD BLUR???',
+            hasDisplayLoaded,
+            hasImgBeenChecked,
+            imgAllowState
+        );
+
+    const imgClass = shouldBlur ? styles.imageboxblur : styles.imagebox;
+    console.log('img class', imgClass);
+
+    const imgSrc = processDisplayValue(display);
+
     return (
         <div className={styles['display-container']}>
-            {!hasDisplayLoaded && (
-                <div className={styles.imagebox} id="pleaseWaitImage">
-                    Please wait for display to load
-                </div>
-            )}
+            {!hasDisplayLoaded ||
+                (hasDisplayLoaded && !imgAllowState && (
+                    <div className={styles.imagebox} id="pleaseWaitImage">
+                        image loading...
+                    </div>
+                ))}
             {hasFailedToLoad ? (
                 <div className={styles.imagebox} id="noImage">
                     No Image was Found
@@ -44,10 +95,11 @@ function DisplayBox({ display }: { display: string | { bytes: number[] } }) {
             ) : (
                 <img
                     id="loadedImage"
-                    className={styles.imagebox}
+                    className={imgClass}
                     style={imageStyle}
                     alt="NFT"
-                    src={processDisplayValue(display)}
+                    // TODO - substitute auto-mod block image for src
+                    src={imgSrc}
                     onLoad={handleImageLoad}
                     onError={handleImageFail}
                 />
